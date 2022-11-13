f8129fd974a1af96ccbaa817001af9b26273952a
Svitlana Synytsia <svetlankogr@gmail.com>
2022-11-13 18:40:18 +0100

    finish task03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,38 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  let delay = Number(form.elements.delay.value);
+  let step = Number(form.elements.step.value);
+  let amount = Number(form.elements.amount.value);
+  for (let i = 0; i < amount; i += 1) {
+    createPromise(i + 1, delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+    delay += step;
   }
+  event.currentTarget.reset();
+}
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      shouldResolve
+        ? resolve({ position, delay })
+        : reject({ position, delay });
+    }, delay);
+  });
 }
